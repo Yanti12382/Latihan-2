f844c3a22c9a2802a835c1b6dbd4427ea6828c75
Yanti12382 <widyayantisiti@gmail.com>
2025-07-11 13:48:13 +0700

    Belajar kondisi

---
+++ belajar kondisi.py
@@ -0,0 +1,14 @@
+# Belajar Kondisi
+
+nilai = int(input("Masukkan nilai Anda: "))
+
+if nilai >= 90:
+    print("Nilai Anda A")
+elif nilai >= 80:
+    print("Nilai Anda B")
+elif nilai >= 70:
+    print("Nilai Anda C")
+elif nilai >= 60:
+    print("Nilai Anda D")
+else:
+    print("Nilai Anda E")
